2084a88a6988aacd3aab08e4d8ca9f0acdf052b0
Tsotne91 <tsotne91@gmail.com>
2021-10-10 23:59:01 +0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        int []arr;
+        arr = new int[number+1];
+        arr [0]=0; arr[1]=1;
+        for (int i=2; i<number; i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        System.out.println(arr[number-1]);
+    }
+}
