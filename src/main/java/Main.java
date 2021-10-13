dc5f30832968d82a6ab4d5d863961280a1dc16ca
Tsotne91 <tsotne91@gmail.com>
2021-10-13 23:50:53 +0400

    fibonacci-tsotne-passingNumberAsArgument

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+        int number = Integer.parseInt(args[0]);
         if (number==0) System.out.println('0');
         else {
             int[] arr;
